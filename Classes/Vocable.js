28027678c57249c4ac88ada2046ac81b9bf89ef8
probook <d.p.arnold@gmx.de>
2021-02-07 11:52:34 +0100

    firstTest

---
+++ Classes/Vocable.js
@@ -0,0 +1,22 @@
+"use strict";
+
+class Vocable{
+    constructor(native, foreign){
+        this.native = native;
+        this.foreign = foreign;
+    }
+    testForeign(foreign){
+        if(foreign === this.foreign){
+            return true;
+        }
+        else{return false;}
+    }
+    testNative(native){
+        if(nativ === this.foreign){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ Classes/Vocabulary.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Vocabulary{
+    constructor(name){
+        this.name = name;
+    }
+    loadFromJSON(){}
+    loadFromExcel(){}
+    loadFromCSV(){}
+    exportToJSON(){}
+}
